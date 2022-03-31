a9eb4432148ee346a6c9b4c4b3c8b8dcbd3253f8
Danton de Lima <dantondelima.contato@gmail.com>
2022-03-31 12:07:34 -0300

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                         </div>
                     @endif
 
-                    <h1>Bem-vindo a 1job!</h1>
+                    <h1>Bem-vindo a 1job!!</h1>
                     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                             <img src="{{ asset('images\1-job.png') }}" alt="">
