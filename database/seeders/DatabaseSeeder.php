a6fa60b20994089f6c8523f3fad1f0c0ca955653
Danton de Lima <dantondelima.contato@gmail.com>
2022-03-31 11:00:19 -0300

    atualizando projeto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => '1job',
+            'email' => 'usuario@1job.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1j0b#2022'), // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
