7a35de12921c5b7d8bab8a3c010c930e39702076
Danton de Lima <dantondelima.contato@gmail.com>
2022-05-03 23:09:05 -0300

    feat: empresa em andamento

---
+++ database/migrations/2022_05_04_000114_create_empresas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cnpj')->unique();
+            $table->string('razao_social');
+            $table->string('nome_fantasia');
+            $table->string('inscricao_estadual');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedInteger('endereco_id');
+            $table->foreign('endereco_id')->references('id')->on('enderecos');
+            // $table->string('nome_responsavel');
+            // $table->string('email_responsavel');
+            // $table->string('telefone');
+            // $table->string('celular');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
