be36f3761d57a6915405c4cdef0732916f9fa61d
Danton de Lima <dantondelima.contato@gmail.com>
2022-05-24 17:52:44 -0300

    feat(admin): recrutador

---
+++ app/Models/Recrutador.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recrutador extends Model
+{
+    use HasFactory;
+}
